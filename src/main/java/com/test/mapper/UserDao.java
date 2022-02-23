7569d82c0c686ddcdbaf1965ba769fb9044c42bc
董合山 <HESHAN.DONG@HAND-CHINA.COM>
2022-02-23 10:35:16 +0800

    first test!

---
+++ src/main/java/com/test/mapper/UserDao.java
@@ -0,0 +1,19 @@
+package com.test.mapper;
+
+import com.test.pojo.User;
+
+public interface UserDao {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(User user);
+
+    int insertSelective(User user);
+
+    User selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(User user);
+
+    int updateByPrimaryKey(User user);
+
+    int delete(User user);
+}
